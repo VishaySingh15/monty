d5ff73cd24c33713367f6fe8364fb75e0f0a3b2d
Vishay <vishay1998@gmail.com>
2023-05-25 06:52:16 -0700

    task 4

--- check_opcode.c
+++ check_opcode.c
@@ -28,7 +28,8 @@ void (*valid_opcode(char *opcode))(stack_t **stack, unsigned int line_number)
 	instruction_t opcodes[] = {
 		{"push", _push}, {"pall", _pall},
 		{"pint", _pint}, {"pop", _pop}, 
-		{"swap", _swap}, {NULL, NULL},
+		{"swap", _swap}, {"add", _add}, 
+		{NULL, NULL},
 	};
 	int count = 0;
 
---
+++ opcode_funcs2.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+void _add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first, *second;
+
+	if (*stack && (*stack)->next)
+	{
+		first = *stack;
+		second = first->next;
+		first->n = first->n + second->n;
+		first->next = second->next;
+		free(second);
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		global_vars.opcode_err = 1;
+	}
+}
